b5b0769594882a46b28f599a43cced23406d01f9
Kevin Beronilla <kevin.beronilla@gmail.com>
2022-09-13 12:03:27 -0500

    Finished hero desktop

--- src/App.js
+++ src/App.js
@@ -23,34 +23,34 @@ function App() {
                         <img src={logo} alt="Kevin Beronilla" className="kb-hero__logo" />
                         <ul className="kb-hero__links">
                             <li>
-                                <a href="./downloads/kevin-beronilla-resume.pdf" target="_blank">
+                                <a className="kb-hero__link" href="./downloads/kevin-beronilla-resume.pdf" target="_blank">
                                     <span className="icon fa fa-file-text"></span>
-                                    <span className="kb-assistive-text">Resume</span>
+                                    <span className="kb-hero__link-label">Resume</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://github.com/kevinberonilla" target="_blank" rel="noreferrer">
+                                <a className="kb-hero__link" href="https://github.com/kevinberonilla" target="_blank" rel="noreferrer">
                                     <span className="icon fa fa-github"></span>
-                                    <span className="kb-assistive-text">GitHub</span>
+                                    <span className="kb-hero__link-label">GitHub</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="mailto:kevin.beronilla@gmail.com">
+                                <a className="kb-hero__link" href="mailto:kevin.beronilla@gmail.com">
                                     <span className="icon fa fa-linkedin"></span>
-                                    <span className="kb-assistive-text">LinkedIn</span>
+                                    <span className="kb-hero__link-label">LinkedIn</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="mailto:kevin.beronilla@gmail.com">
+                                <a className="kb-hero__link" href="mailto:kevin.beronilla@gmail.com">
                                     <span className="icon fa fa-envelope"></span>
-                                    <span className="kb-assistive-text">Email</span>
+                                    <span className="kb-hero__link-label">Email</span>
                                 </a>
                             </li>
                         </ul>
                     </div>
                     <div className="kb-hero__content">
                         <h1 className="kb-text-heading kb-text-heading--large">
-                            My name is <span className="kb-font-color--brand">Kevin Beronilla</span> and I am a visual artist.
+                            My name is<br /><span className="kb-font-color--brand">Kevin Beronilla</span><br />and I am a visual artist.
                         </h1>
                         <p className="kb-hero__description">With a multi-disciplinary background in graphic/UI/UX design, front-end development, photography, and video, I aim to create beautiful experiences in multi-media.</p>
                     </div>
