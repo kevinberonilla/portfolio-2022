690386ef23c47f3ca15e39871d46200f6e4038d8
Kevin Beronilla <kevin.beronilla@gmail.com>
2022-09-14 15:56:04 -0500

    Focus states; checkbox design

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -2,20 +2,24 @@ import { useRef } from 'react';
 import PropTypes from 'prop-types';
 
 function Checkbox(props) {
-    const root = useRef();
+    const randomId = 'checkbox-' + Math.random().toString().replace('.', '');
 
     return (
-        <label ref={root} className={'kb-checkbox' + (props.className ? ' ' + props.className : '')}>
-            <input type="checkbox" name={props.name} checked={props.checked} onChange={props.onChange} />
-            <div className="kb-checkbox__label kb-text--small">{props.label}</div>
-        </label>
+        <div className={'kb-checkbox' + (props.className ? ' ' + props.className : '')}>
+            <input id={props.id ? props.id + '-' + randomId : randomId} className="kb-checkbox__input" type="checkbox" name={props.name} checked={props.checked} onChange={props.onChange} />
+            <label className="kb-checkbox__layout" for={props.id ? props.id + '-' + randomId : randomId}>
+                <div className="kb-checkbox__faux"></div>
+                <div className="kb-checkbox__label kb-text--small">{props.label}</div>
+            </label>
+        </div>
     );
 }
 
 Checkbox.propTypes = {
+    id: PropTypes.string,
     label: PropTypes.string,
     name: PropTypes.string,
-    checked: PropTypes.bool
+    checked: PropTypes.bool,
 }
 
 export default Checkbox;
