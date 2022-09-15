8a056ea2999864220b4f46534aa30e3de04eb49f
Kevin Beronilla <kevin.beronilla@gmail.com>
2022-09-15 16:46:19 -0500

    Finished desktop experience

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,34 +1,54 @@
-import { useState, useRef } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 function Carousel(props) {
     const carousel = useRef();
     const [activeIndex, setActiveIndex] = useState(0);
 
-    function handleCarouselScroll() {
-        setActiveIndex(Math.round(carousel.current.scrollLeft / carousel.current.clientWidth));
-    }
-
-    function next() {
-        if (activeIndex === props.images.length - 1) {
-            carousel.current.scrollLeft = 0;
-        } else {
-            carousel.current.scrollLeft += carousel.current.clientWidth;
-        }
-    }
-
-    function back() {
+    const back = useCallback(() => {
         if (activeIndex === 0) {
             carousel.current.scrollLeft = carousel.current.clientWidth * (props.images.length - 1);
         } else {
             carousel.current.scrollLeft -= carousel.current.clientWidth;
         }
+    }, [activeIndex, props.images.length]);
+
+    const next = useCallback(() => {
+        if (activeIndex === props.images.length - 1) {
+            carousel.current.scrollLeft = 0;
+        } else {
+            carousel.current.scrollLeft += carousel.current.clientWidth;
+        }
+    }, [activeIndex, props.images.length]);
+
+    useEffect(() => {
+        function handleKeyup(event) {
+            if (event.key === 'ArrowLeft') {
+                back();
+            } else if (event.key === 'ArrowRight') {
+                next();
+            }
+        }
+
+        window.addEventListener('keyup', handleKeyup);
+
+        return () => {
+            window.removeEventListener('keyup', handleKeyup);
+        }
+    }, [back, next]);
+
+    function handleCarouselScroll() {
+        setActiveIndex(Math.round(carousel.current.scrollLeft / carousel.current.clientWidth));
+    }
+    
+    function goToIndex(index) {
+        carousel.current.scrollLeft = carousel.current.clientWidth * index;
     }
 
-    function goToIndex(event) {
+    function handleNavigationClick(event) {
         const index = parseInt(event.currentTarget.dataset.index, 10);
-
-        carousel.current.scrollLeft = carousel.current.clientWidth * index;
+        
+        goToIndex(index);
     }
 
     return (
@@ -48,19 +68,17 @@ function Carousel(props) {
                 props.images.length > 1
                 ?
                 <>
-                    <button className="kb-carousel__back fa-solid fa-chevron-left" onClick={back}></button>
-                    <button className="kb-carousel__next fa-solid fa-chevron-right" onClick={next}></button>
                     <div className="kb-carousel__nav">
                         {
                             props.images.map((image, imageIndex) => {
                                 return (
-                                    <span key={image} data-index={imageIndex} onClick={goToIndex}>
-                                        {imageIndex === activeIndex ? 'Yes' : 'No'}
-                                    </span>
+                                    <div key={image} className={'kb-carousel__nav-item' + (imageIndex === activeIndex ? ' kb-carousel__nav-item--active' : '')} data-index={imageIndex} onClick={handleNavigationClick}></div>
                                 )
                             })
                         }
                     </div>
+                    <button className="kb-carousel__back fa-solid fa-chevron-left" onClick={back}></button>
+                    <button className="kb-carousel__next fa-solid fa-chevron-right" onClick={next}></button>
                 </>
                 :
                 ''
